490b9eb9041e77db7e71b91d512b48f561b0011e
adxamoov07 <adxamovabdulloh06@gmail.com>
2023-06-24 13:42:03 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+let btn = document.querySelector(".button1")
+let h1 = document.querySelector("h1")
+let btn2 = document.querySelector(".banner")
+let ppp = document.querySelector("p")
+let bbb = document.querySelector("b")
+let divv = document.querySelector(".banner_child_item")
+
+
+function changeColor(){
+    h1.style.color = "black"
+    btn2.style.background = "white"
+    ppp.style.color = "black"
+    bbb.style.color = "black"
+    divv.style.background = "black"
+    btn.style.background = "#fff"
+}
+
+let button2 = document.querySelector(".button2")
+let p = document.querySelector("h1")
+let btn1 = document.querySelector(".banner")
+let pp = document.querySelector("p")
+let bb = document.querySelector("b")
+let div = document.querySelector(".banner_child_item")
+
+
+
+function changeColor1(){
+    button2.style.background = "#222"
+    p.style.color = "white"
+    btn1.style.background = "#222"
+    pp.style.color = "white"
+    bb.style.color = "white"
+    div.style.background = "white"
+
+
+}
